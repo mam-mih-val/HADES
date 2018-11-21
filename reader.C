9862699d1385e1a0f8634cc0fa11779ac4531d1a
Mikhail Mamaev <mikhail@mikhail.Mikhail>
2018-11-22 00:00:45 +0300

    q_vs_tracks added

--- reader.C
+++ reader.C
@@ -11,21 +11,38 @@ void reader()
     auto DTEvent = (TBranch*) t->GetBranch("DTEvent");
     DTEvent->SetAddress(&ev);
 
-    TH1F* multy_MDC = new TH1F("number of tracks, MDC","number of tracks MDC & hits TOF+RPC",100,1,100);
-    multy_MDC->SetLineColor(1);
+    TH1F* multy_MDC = new TH1F("number of tracks, MDC","number of tracks MDC",100,1,100);
+//    multy_MDC->SetLineColor(1);
+    multy_MDC->GetXaxis()->SetTitle("MDC tracks");
     TH1F* multy_TOF = new TH1F("number of hits, TOF","number of hits, TOF+RPC",100,1,100);
-    multy_TOF->SetLineColor(2);
+//    multy_TOF->SetLineColor(2);
+    multy_TOF->GetXaxis()->SetTitle("TOF hits");
+    TH2F* q_vs_tr = new TH2F("charge vs tracks","charge in FW vs number of tracks",100,1,100,100,1,8000);
+    q_vs_tr->GetXaxis()->SetTitle("MDC tracks");
+    q_vs_tr->GetYaxis()->SetTitle("FW charge");
     Long64_t n_events = (Long64_t) t->GetEntries();
     Int_t n_tracks = 0;
     Int_t n_hits = 0;
+    Double_t charge = 0;
     for(int i=0;i<n_events;i++)
     {
         DTEvent->GetEntry(i);
         n_tracks = ev->GetNVertexTracks();
         n_hits = ev->GetNTOFHits();
+        charge = ev->GetPSDEnergy();
+
         multy_MDC->Fill(n_tracks);
         multy_TOF->Fill(n_hits);
+        q_vs_tr->Fill(n_tracks,charge);
     }
-    multy_MDC->Draw();
-    multy_TOF->Draw("same");
+
+    q_vs_tr->Draw("colz");
+    TFile* w = new TFile("histo.root","recreate");
+    w->cd();
+    multy_MDC->Write();
+    multy_TOF->Write();
+    q_vs_tr->Write();
+    style->Write();
+    w->Close();
+
 }
