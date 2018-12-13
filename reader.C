fc9b6e3deefb370df0b2d3e20aef1baca366ee53
Mikhail Mamaev <mikhail@mikhail.Mikhail>
2018-12-13 18:38:50 +0300

    Q, Resolution & directed flow histos

--- reader.C
+++ reader.C
@@ -3,20 +3,20 @@
 reader::reader()
 {
     fChain = new TChain("DataTree");
-    fChain->Add("~/pool/1/mam2mih/out.root");
+    fChain->Add("AuAu.root");
     ev = new DataTreeEvent;
     DTEvent = (TBranch*) fChain->GetBranch("DTEvent");
     DTEvent->SetAddress(&ev);
 
-    MeanQx = new TProfile("c vs Qx","centrality vs Qx",10,0,100);
+    MeanQx = new TProfile("c vs Qx","centrality vs Qx",20,0,100);
     MeanQx->GetYaxis()->SetTitle("<Qx>");
     MeanQx->GetXaxis()->SetTitle("centrality");
 
-    MeanQy = new TProfile("c vs Qy","centrality vs Qy",10,0,100);
+    MeanQy = new TProfile("c vs Qy","centrality vs Qy",20,0,100);
     MeanQy->GetYaxis()->SetTitle("<Qy>");
     MeanQy->GetXaxis()->SetTitle("centrality");
 
-    ResPsi = new TProfile("resolution of SE, r","Resolution of subevent method, recentred",10,0,100);
+    ResPsi = new TProfile("resolution of SE, r","Resolution of subevent method, recentred",20,0,100);
 }
 reader::~reader()
 {
@@ -33,12 +33,12 @@ void reader::Q_histos()
     this->GetQMean();
     Float_t* Q = new Float_t[2];
     Int_t n_ev = fChain->GetEntries();
-    TProfile* Qx_vs_c = new TProfile("Qx vs c, recentred", "Qx vs centrality after recentring",10,0,100);
-    TProfile* Qy_vs_c = new TProfile("Qy vs c, recentred", "Qy vs centrality after recentring",10,0,100);
+    TProfile* Qx_vs_c = new TProfile("Qx vs c, recentred", "Qx vs centrality after recentring",20,0,100);
+    TProfile* Qy_vs_c = new TProfile("Qy vs c, recentred", "Qy vs centrality after recentring",20,0,100);
     TH1F* Qx_nr_histo = new TH1F("Qx nr","Qx not recented",100,-1,1);
     TH1F* Qx_r_histo = new TH1F("Qx r","Qx recented",100,-1,1);
     TH1F* Qy_nr_histo = new TH1F("Qy nr","Qy not recented",100,-1,1);
-    TH1F* Qy_r_histo = new TH1F("Qy r","Qx recented",100,-1,1);
+    TH1F* Qy_r_histo = new TH1F("Qy r","Qy recented",100,-1,1);
     Float_t cent;
     Float_t meanx, meany;
     Int_t bin;
@@ -48,7 +48,9 @@ void reader::Q_histos()
         if(ev->GetPSDEnergy()==0)
             continue;
         cent = ev->GetCentrality();
-        bin = cent/10+1;
+        if(cent<0)
+            continue;
+        bin = cent/5+1;
         meanx = MeanQx->GetBinContent(bin);
         meany = MeanQy->GetBinContent(bin);
         Q = this->GetQ();
@@ -58,7 +60,7 @@ void reader::Q_histos()
         Qy_r_histo->Fill(Q[1]-meany);
         Qx_vs_c->Fill(cent,Q[0]-meanx);
         Qy_vs_c->Fill(cent,Q[1]-meany);
-        cout << i/n_ev << endl;
+        this->ShowProgress(i,n_ev);
     }
     TFile* f = new TFile("histos.root","recreate");
     f->cd();
@@ -76,7 +78,7 @@ void reader::v_histos()
 {
     this->GetQMean();
     this->GetResPsi();
-    auto ResSubEv_nr = new TProfile("Res of SE, nr","Resolution of Subevent, not recentred",10,0,100);
+    auto ResSubEv_nr = new TProfile("Res of SE, nr","Resolution of Subevent, not recentred",20,0,100);
     Int_t n_ev = fChain->GetEntries();
     Float_t* PsiEP_nr = new Float_t[2];
     Float_t  cent;
@@ -87,7 +89,9 @@ void reader::v_histos()
         if(ev->GetPSDEnergy() == 0)
             continue;
         cent=ev->GetCentrality();
-        PsiEP_nr = this->GetPsiEP(0);
+        if (cent < 0)
+            continue;
+        PsiEP_nr = this->GetPsiEP_SE(0);
         Float_t res_nr = cos(PsiEP_nr[0]-PsiEP_nr[1]);
         ResSubEv_nr->Fill(cent,res_nr);
     }
@@ -98,14 +102,16 @@ void reader::v_histos()
     f->Close();
 }
 
-Float_t reader::Get_v( Float_t* PsiEP, Float_t phi, Int_t n=1  )
+Float_t reader::Get_v( Float_t PsiEP, Float_t phi, Int_t n=1  )
 {
-    Float_t MeanPsi = (PsiEP[0]+PsiEP[1])/2;
     Float_t cent = ev->GetCentrality();
-    Int_t bin = (cent/10)+1;
+    Int_t bin = (cent/5)+1;
+    //cout << bin << endl;
     Float_t res = ResPsi->GetBinContent(bin);
     Float_t v_n=0;
-    v_n = cos( n*(phi-MeanPsi) )/sqrt(res);
+    v_n = cos( n*(phi-PsiEP) ) / sqrt(res);
+    if(v_n != v_n)
+        cout << "PsiEP=" << PsiEP << " phi=" << phi << " res=" << res << " v1=" << v_n << "centrality =" << cent <<endl;
     return v_n;
 }
 
@@ -114,11 +120,19 @@ void reader::GetFlows()
     this->GetQMean();
     this->GetResPsi();
     cout <<"building pt&y vs directed flow"<<endl;
-    auto y_vs_v1 = new TProfile("y vs v1","rapidity vs directed flow, protons",8,-1,2);
-    auto pt_vs_v1 = new TProfile("pt vs v1","transverse momentum vs directed flow, protons",8,0.2,2.0);
+
+    auto y_vs_v1_cent = new TProfile("y vs v1, 0-20","rapidity vs directed flow, centrality 0-20\%, protons",15,-1,2);
+    auto pt_vs_v1_cent = new TProfile("pt vs v1, 0-20","transverse momentum vs directed flow, centrality 0-20\%, protons",10,0.2,2.0);
+    
+    auto y_vs_v1_mid = new TProfile("y vs v1, 20-30","rapidity vs directed flow, centrality 20-30\%, protons",15,-1,2);
+    auto pt_vs_v1_mid = new TProfile("pt vs v1, 20-30","transverse momentum vs directed flow, centrality 20-30\%, protons",10,0.2,2.0);
+
+    auto y_vs_v1_per = new TProfile("y vs v1, 30-50","rapidity vs directed flow, centrality 30-50\%, protons",15,-1,2);
+    auto pt_vs_v1_per = new TProfile("pt vs v1, 30-50","transverse momentum vs directed flow, centrality 30-50\%, protons",10,0.2,2.0);
+
     Int_t n_ev = fChain->GetEntries();
     DataTreeTrack* tr;
-    Float_t* PsiEP = new Float_t[2];
+    Float_t PsiEP;
     Float_t pt, y, w, phi;
     Float_t cent;
     Float_t v1;
@@ -133,6 +147,10 @@ void reader::GetFlows()
         if (n_tr < 5)
             continue;
         cent = ev->GetCentrality();
+        if(cent < 0)
+        {
+            continue;
+        }
         for(int j=0;j<n_tr;j++)
         {
             tr = ev->GetVertexTrack(j);
@@ -147,15 +165,32 @@ void reader::GetFlows()
                 w=1;
             if(y<0)
                 w=-1;
-            y_vs_v1->Fill(y,v1);
-            pt_vs_v1->Fill(pt,w*v1);
+            if (cent < 20) 
+            {
+                y_vs_v1_cent->Fill(y,v1);
+                pt_vs_v1_cent->Fill(pt,w*v1);
+            }
+            if( (cent>=20)&&(cent<30) )
+            {
+                y_vs_v1_mid->Fill(y,v1);
+                pt_vs_v1_mid->Fill(pt,w*v1);
+            }
+            if( (cent>=30)&&(cent<50) )
+            {
+                y_vs_v1_per->Fill(y,v1);
+                pt_vs_v1_per->Fill(pt,w*v1);
+            }
         }
         this->ShowProgress(i,n_ev);
     }
     TFile* f = new TFile("histos_flow.root","recreate");
     f->cd();
-    y_vs_v1->Write();
-    pt_vs_v1->Write();
+    y_vs_v1_cent->Write();
+    pt_vs_v1_cent->Write();
+    y_vs_v1_mid->Write();
+    pt_vs_v1_mid->Write();
+    y_vs_v1_per->Write();
+    pt_vs_v1_per->Write();
     f->Close();
 }
 
@@ -171,15 +206,18 @@ void reader::GetQMean()
         if( ev->GetPSDEnergy() == 0 )
             continue;
         cent = ev->GetCentrality();
+        if( cent < 0)
+            continue;
         Q = this->GetQ();
         MeanQx->Fill(cent, Q[0]);
         MeanQy->Fill(cent, Q[1]);
+        this->ShowProgress(i,N_ev);
     }
 }
 
 void reader::GetResPsi()
 {
-    cout << "Fillin subevent resolution function of centrality" << endl;
+    cout << "Filling subevent resolution function of centrality" << endl;
     Int_t n_ev = fChain->GetEntries();
     Float_t* PsiEP = new Float_t[2];
     Float_t  cent;
@@ -189,17 +227,18 @@ void reader::GetResPsi()
         if(ev->GetPSDEnergy() == 0)
             continue;
         cent = ev->GetCentrality();
-        PsiEP = this->GetPsiEP();
+        PsiEP = this->GetPsiEP_SE();
         Float_t res = cos(PsiEP[0]-PsiEP[1]);
         ResPsi->Fill(cent,res);
+        this->ShowProgress(i,n_ev);
     }
 }
 
-Float_t* reader::GetPsiEP(Bool_t recentring=1) // random subevent method
+Float_t* reader::GetPsiEP_SE(Bool_t recentring=1) // random subevent method
 {
     Float_t cent = ev->GetCentrality();
     Float_t meanx = 0, meany=0;
-    Int_t bin = (cent/10)+1;
+    Int_t bin = (cent/5)+1;;
     Float_t Qx[2], Qy[2];
     meanx = MeanQx->GetBinContent(bin);
     meany = MeanQy->GetBinContent(bin);
@@ -215,6 +254,8 @@ Float_t* reader::GetPsiEP(Bool_t recentring=1) // random subevent method
     for(int i=0;i<n_psd_modules;i++)
     {
         psd_module = ev->GetPSDModule(i);
+        if(psd_module->GetId() < 0)
+            continue;
         phi =        psd_module->GetPhi();
         q   =        psd_module->GetEnergy();
         Int_t p = gRandom->Uniform(0,2);
@@ -238,6 +279,20 @@ Float_t* reader::GetPsiEP(Bool_t recentring=1) // random subevent method
     }
     return PsiEP;
 }
+
+Float_t reader::GetPsiEP()
+{
+    Float_t* Q =(Float_t*) this->GetQ();
+    Float_t cent = ev->GetCentrality();
+    Int_t bin = (cent/5) + 1;
+    Float_t meanx=MeanQx->GetBinContent(bin);
+    Float_t meany=MeanQy->GetBinContent(bin);
+    Q[0]-=meanx; Q[1]-=meany;
+//    cout << "Qx=" << Q[0] << " Qy=" << Q[1] << endl;
+    Float_t PsiEP=atan2(Q[1],Q[0]);
+    return PsiEP;
+}
+
 Bool_t  reader::evSelector()
 {
     if (ev->GetNVertexTracks() < 5)
@@ -267,6 +322,8 @@ Float_t* reader::GetQ()
     for(int j=0;j<n_psd_modules;j++)
     {
         psd_module = ev->GetPSDModule(j);
+        if(psd_module->GetId() < 0)
+            continue;
         phi =        psd_module->GetPhi();
         q   =        psd_module->GetEnergy();
         Qx+= q*cos(phi); 
@@ -280,7 +337,7 @@ Float_t* reader::GetQ()
 void reader::ShowProgress(Int_t i, Int_t N)
 {
     Float_t barsize=50;
-    Int_t progress = barsize*i/N;
+    Int_t progress = barsize*i/N-1;
     if(i == 0)
     {
         cout << "[" << flush;
@@ -288,7 +345,7 @@ void reader::ShowProgress(Int_t i, Int_t N)
             cout << "." << flush;
         cout << "]" << flush;
     }
-    if( (i!=0)&&(i!=N) )
+    if( (i>0)&&(i<N-2) )
     {
         for(int j=0;j<barsize;j++)
             cout<<"\r"<<flush;
@@ -299,7 +356,7 @@ void reader::ShowProgress(Int_t i, Int_t N)
             cout<<"."<<flush;
         cout<<"]"<<flush;
     }
-    if( i==N )
+    if( i>N-2 )
     {
         for(int j=0;j<barsize;j++)
             cout<<"\r"<<flush;
--- reader.h
+++ reader.h
@@ -13,8 +13,9 @@ class reader
     void    GetEvent(Int_t n);
     Bool_t  evSelector();
     Bool_t  Check_track( Int_t tr_num );
-    Float_t Get_v(Float_t* PsiEP, Float_t phi, Int_t n=1);
-    Float_t* GetPsiEP(Bool_t recentring=1);
+    Float_t Get_v(Float_t PsiEP, Float_t phi, Int_t n=1);
+    Float_t* GetPsiEP_SE(Bool_t recentring=1);
+    Float_t GetPsiEP();
     Float_t* GetQ();
     void    GetResPsi();
     void    Q_histos();
